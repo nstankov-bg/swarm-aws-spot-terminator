20d0ace9b3f06addc57a1505fd6ea882f61d1ad9
Nikolay Stankov <me@nstankov.com>
2021-07-06 17:13:06 -0400

    nikoogle dockerhub

--- publish.sh
+++ publish.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 DOCKER_HOST=unix:///var/run/docker.sock
-IMAGE=shapigor/swarm-aws-spot-terminator
+IMAGE=nikoogle/swarm-aws-spot-terminator
 VERSION=latest
 TAG="$IMAGE:$VERSION"
 docker build -t $TAG .
